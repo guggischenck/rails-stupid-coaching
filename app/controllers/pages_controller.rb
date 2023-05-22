c7a43b725c4a8f925fc6217d09c437fe507c57ba
Gustav Schenck <gugi.schenck@icloud.com>
2023-05-22 14:53:14 +0200

    done

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+  @members = ["thanh", "dimitri", "germain", "damien", "julien"]
+   if params[:member]
+     @members = @members.select { |member| member.start_with?(params[:member]) }
+  end
+end
